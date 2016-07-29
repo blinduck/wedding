9eb723bf0fc6b0b38225520a2b4550cf0e86d777
deepan <blinduck@gmail.com>
2016-07-29 21:58:34 +0800

    added map and some other stuff

--- index.js
+++ index.js
@@ -6,25 +6,59 @@ var BasicDataComponent = {
     view: function () {
        return (m('div.row', [
            m("div.col-xs-12", [
-               m("p", "November 6th"),
-               m("p", "Open Farm Community"),
-               m("p", "Guests to start arriving at 5PM"),
-               m("p", "Ceremony starts at 5.30PM on the dot")
+               m("img#main-image.img-rounded", {src:'hitched.gif'}),
+               m("h3#main-header.cursive.text-center", m.trust('Deepan & Julianne <br> Get Hitched')),
+               m("div.text-center", [
+                   m("p", "Friday, November 6th 2016"),
+                   m("p", "CEREMONY STARTS AT 530PM")
+               ]),
+
            ])
        ]))
     }
 
 
-}
+};
 
 var NavComponent = {
     controller: function () {
-
+        this.headers = [
+            {route: '/', text: 'home'},
+            {route: '/location', text: 'location'},
+            {route: '/rsvp', text: 'rsvp'}
+        ]
     },
-    view: function (ctrl) {
-        return m("div", 'testing')
+    view: function(ctrl) {
+        return(m("ul", ctrl.headers.map(function(header){
+            return (m('li', {class: m.route() == header.route ? 'active' : ''}, [
+                m("a[href='" + header.route + "']",
+                    {config: m.route},
+                    header.text)
+            ]))
+        })))
     }
 
-}
+};
 
-m.mount(document.getElementById('content'), BasicDataComponent);
+var RSVPComponent = {
+    view: function (ctrl) {
+        return m.trust('<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdT9xiirANiKVjxLEpm_AfGnIRnpJPjFRe0_Q2bjkXwM2VZ3A/viewform?embedded=true" width="100%" height="1000px" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>')
+    }
+};
+
+var LocationComponent = {
+    view: function (ctrl) {
+        return m('div.text-center', [
+            m.trust('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5480.40373304769!2d103.81478595870996!3d1.3049512767508944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da1a224e7f6a3b%3A0x3c9a08c69ab33461!2sOpen+Farm+Community!5e0!3m2!1sen!2ssg!4v1469800540533" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'),
+            m("p", 'someting')
+        ])
+    }
+
+};
+
+m.mount(document.getElementById('nav'),NavComponent);
+m.route(document.getElementById('content'), '/', {
+    "/": BasicDataComponent,
+    "/location": LocationComponent,
+    "/rsvp": RSVPComponent
+});
