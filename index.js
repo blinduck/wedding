46c9025db466d80450ec171a6df244d4ab8b6f24
deepan <blinduck@gmail.com>
2016-07-30 18:12:14 +0800

    changed some text

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ var BasicDataComponent = {
            m("div.col-xs-12", [
                m("img#main-image.img-rounded", {src:'hitched.gif'}),
                m("h3#main-header.cursive.text-center", m.trust('Deepan & Julianne <br> Get Hitched')),
-               m("div.text-center", [
+               m("div.text-center.lead", [
                    m("p", "Friday, November 6th 2016"),
-                   m("p", "CEREMONY STARTS AT 530PM")
+                   m("p", "Open Farm Community"),
+                   m("p", "130E Minden Rd, Singapore 248819"),
+                   m("p", "Please arrive by 5. Ceremony starts at 530."),
+                   m("p", "Outdoor solemnization followed by indoor dinner"),
+                   m('p', "Free Flow Beer & Wine (Don't drive?)"),
                ]),
 
            ])
@@ -24,7 +28,7 @@ var NavComponent = {
     controller: function () {
         this.headers = [
             {route: '/', text: 'home'},
-            {route: '/location', text: 'location'},
+            {route: '/location', text: 'map'},
             {route: '/rsvp', text: 'rsvp'}
         ]
     },
@@ -50,7 +54,6 @@ var LocationComponent = {
     view: function (ctrl) {
         return m('div.text-center', [
             m.trust('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5480.40373304769!2d103.81478595870996!3d1.3049512767508944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da1a224e7f6a3b%3A0x3c9a08c69ab33461!2sOpen+Farm+Community!5e0!3m2!1sen!2ssg!4v1469800540533" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'),
-            m("p", 'someting')
         ])
     }
 
