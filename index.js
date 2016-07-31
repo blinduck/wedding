d8e674b8a854631cdb2d50639b580727e47a2fd6
deepan <blinduck@gmail.com>
2016-07-31 17:20:04 +0800

    cleaned up

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 
 var BasicDataComponent = {
-    controller: function () {
-
-    },
     view: function () {
        return (m('div', [
            m("div", [
-               m("img#main-image.img-rounded", {src:'hitched.gif'}),
+               m("img#main-image", {src:'https://padlet-uploads.s3.amazonaws.com/prod/71384074/2b5377409403c9d04357b543bcc630832112f26e/1092a8130bea0d9a4025ca7155c1d85b.gif'}),
                m("h3#main-header.cursive.text-center", m.trust('Deepan & Julianne <br> Get Hitched')),
                m("div.text-center.lead", [
                    m("p", "Sunday, November 6th 2016"),
                    m("p", "Open Farm Community"),
                    m("p", "130E Minden Rd, Singapore 248819"),
                    m("p", "Please arrive by 5. Ceremony starts at 530."),
-                   m("p", "Outdoor solemnization followed by indoor dinner"),
+                   m("p", "Outdoor solemnization, followed by indoor dinner."),
                    m('p', "Free Flow Beer & Wine (Consider not driving)"),
+                   m("a[href='/rsvp']",{config: m.route}, "RSVP")
                ]),
 
            ])
        ]))
     }
-
-
 };
 
 var NavComponent = {
@@ -33,13 +29,16 @@ var NavComponent = {
         ]
     },
     view: function(ctrl) {
-        return(m("ul", ctrl.headers.map(function(header){
-            return (m('li', {class: m.route() == header.route ? 'active' : ''}, [
-                m("a[href='" + header.route + "']",
-                    {config: m.route},
-                    header.text)
-            ]))
-        })))
+        return([
+                m("ul.nav-list", ctrl.headers.map(function(header){
+                    return (m('li', {class: m.route() == header.route ? 'active' : ''}, [
+                        m("a[href='" + header.route + "']",
+                            {config: m.route},
+                            header.text)
+                    ]))
+                }))
+            ]
+        )
     }
 
 };
@@ -54,9 +53,16 @@ var LocationComponent = {
     view: function (ctrl) {
         return m('div.text-center', [
             m.trust('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5480.40373304769!2d103.81478595870996!3d1.3049512767508944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da1a224e7f6a3b%3A0x3c9a08c69ab33461!2sOpen+Farm+Community!5e0!3m2!1sen!2ssg!4v1469800540533" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'),
+            m("div", [
+                m("h3", "Nearby Parking:"),
+                m("p", "Off Minden Road, along Sherwood Road"),
+                m("p", "Botanic Gardens, along Tyersall Avenue & Cluny Road"),
+                m("p", "Dempsey Road, Blk 7A, 8D, 11 - 18, 22"),
+                m("p", "Loewen Road, opp Blk 27A")
+            ])
+
         ])
     }
-
 };
 
 m.mount(document.getElementById('nav'),NavComponent);
